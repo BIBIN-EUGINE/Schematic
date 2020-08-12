f3963d280e05801ba7c365aaca2963c98a418ab8
BIBIN EUGINE <36467153+BIBIN-EUGINE@users.noreply.github.com>
2020-08-11 23:09:08 -0400

    Add files via upload

---
+++ display_camera_sdcard/Indesign/StubI.vhd
@@ -0,0 +1,4 @@
+-- Generated PORTMAP Stub File: Created by Capture FPGA Flow
+-- Matches PCB component pinout with simulation model
+-- Created Friday, May 24, 2019 11:25:38 Eastern Daylight Time
+
---
+++ display_camera_sdcard/Indesign/sdf.cmd
@@ -0,0 +1,2 @@
+LOG_FILE = "g:\design\Indesign\sdfout.log",
+SCOPE = :dut;
